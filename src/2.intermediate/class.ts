77e5674e5440cacfe951f13393770991bc38b70b
wangweihua <wangweihua@bytedance.com>
2021-08-15 16:49:52 +0800

    ♻️ add equality assertion

--- src/2.intermediate/class.ts
+++ src/2.intermediate/class.ts
@@ -1,6 +1,6 @@
 import { Equal } from '@type-challenges/utils'
 namespace _class {
-  class TPoint {
+  class Point {
     constructor(public x: number, public y: number) { }
   }
 
@@ -9,6 +9,13 @@ namespace _class {
     new(x: number, y: number): { x: number, y: number };
   }
 
-  type TPointProps = keyof typeof TPoint // "prototype"
-  type R = Equal<typeof TPoint, IPoint>; // true
+  type TPoint = {
+    prototype: {x: number, y: number};
+    new(x: number, y: number): { x: number, y: number };
+  };
+
+  type TPointProps = keyof typeof Point // "prototype"
+  type R1 = Equal<IPoint, typeof Point>; // true
+  type R2 = Equal<TPoint, typeof Point>; // true
+  type R3 = Equal<IPoint, TPoint>; // true
 }
