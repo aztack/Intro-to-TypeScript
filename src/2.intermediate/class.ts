df3f8c0a67a614f75b822503c6c3b626092309ec
wangweihua <wangweihua@bytedance.com>
2021-08-16 15:18:17 +0800

    ♻️ expand class definition

--- src/2.intermediate/class.ts
+++ src/2.intermediate/class.ts
@@ -1,7 +1,12 @@
 import { Equal } from '@type-challenges/utils'
 namespace _class {
   class Point {
-    constructor(public x: number, public y: number) { }
+    x: number = 0;
+    y: number = 0;
+    constructor(x: number,y: number) {
+      this.x = x;
+      this.y = y;
+    }
   }
 
   interface IPoint {
